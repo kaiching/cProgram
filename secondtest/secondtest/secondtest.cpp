e0b94d1941b427f2500e6883e07ede76c022f267
kaiching <xinluomo@163.com>
2016-04-01 17:16:16 +0800

    exercise

--- secondtest/secondtest/secondtest.cpp
+++ secondtest/secondtest/secondtest.cpp
@@ -5,6 +5,13 @@
 #include "stdio.h"
 #include "string.h"
 
+struct node
+{
+	int a;
+	int b;
+	int c;
+};
+
 
 int strcmp(char *src, char *dst) //比较两个字符串是否相等
 {
@@ -57,15 +64,94 @@ char *strcat(char *src, char *dst) //把src所指字符串添加到dst结尾处
 	*cp = '\0';
 	return dst;
 }
-int _tmain(int argc, _TCHAR* argv[])
+
+char *invstr(char *src)  //字符串反转
+{
+	char *p = src;
+	char t;
+	int len = strlen(p);
+	for (int i=0; i<len/2; i++)
+	{
+		t = p[i];
+		p[i] = p[len-1-i];
+		p[len-1-i] = t;
+	}
+	return src;
+}
+
+int foo ( int x , int n)
+{
+	int val;
+	val =1;
+	if (n>0)
+	{
+		if (n%2 == 1) 
+		val = val *x;
+
+		val = val * foo(x*x , n/2);
+	}
+	return val;
+}
+
+void foo( int b[][3])
+{
+	++ b;
+	b[1][1] =9;
+}
+
+void e(int n)
+{
+	if(n>0)
+	{
+		e(--n);
+		printf("%d" , n);
+		e(--n);
+	}
+}
+
+void f( char **p )
+{
+	char* t;
+	t= (p+= sizeof(int))[-1];
+	printf( "%s" , t);
+}
+
+int main()
 {
 // 	char a[] = "123321";
 // 	int c = func(a);
 // 	printf("%d\n", c);
-	char a[] = "123";
-	char b[256] = "67809";
-	char *s = strcat(a,b);
-	printf(s);
+
+// 	char a[] = "123";
+// 	char b[256] = "67809";
+// 	char *s = strcat(a,b);
+// 	printf(s);
+
+// 	char a[] = "helloworld";
+// 	char *s = invstr(a);
+// 	printf(s);
+
+// 	struct node s = {3, 5, 6};
+// 	struct node *pt =&s;
+// 	printf("%d", *((int*)pt+1));
+
+// 	int val = foo(6,5);
+// 	printf("%d", val
+
+// 	int a [3][3]= { { 1,2,3} , { 4,5,6},{7,8,9}};
+// 	foo(a);
+// 	printf("%d" , a[2][1]);
+
+// 	int i=3;
+// 	int j=(++i)+(++i);
+
+// 	int a;
+// 	a=3;
+// 	e(a);
+
+	char * argv[] = { "ab" ,"cd" , "ef" ,"gh", "ij" ,"kl" };
+	f( argv );
+
  	return 0;
 }
 
